cbbff7b1ab7a417f31387c63bec5741c069ea559
Alex Leo <93365612+alexleo97@users.noreply.github.com>
2022-02-05 01:01:59 +0000

    Added basic html/css and added openweathermap api

---
+++ app.js
@@ -0,0 +1,25 @@
+window.addEventListener("load", () => {
+  let long;
+  let lat;
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(position => {
+        long = position.coords.longitude;
+        lat = position.coords.latitude;
+
+        const api = `api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=ff3aebc189272a372a166ca3dbeba897`;
+
+
+        fetch(api)
+        .then(response =>{
+          return response.json();
+        })
+        .then(data => {
+          console.log(data);
+        });
+      });
+
+
+      
+    }
+});
